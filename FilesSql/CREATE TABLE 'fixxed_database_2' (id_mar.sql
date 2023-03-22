324ccbbc177dc8b6e56d519a1a61bc627add8604
Hydeoshy Kaled <hydeoshy.kaled65@gmail.com>
2023-03-21 23:29:19 -0300

    first commit

---
+++ FilesSql/CREATE TABLE 'fixxed_database_2' (id_mar.sql
@@ -0,0 +1,12 @@
+CREATE TABLE 'fixxed_database_2' (id_marca INTEGER,marca TEXT);
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('1', 'Fiat');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('2', 'Volkswagen');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('3', 'Kia');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('4', 'Peugeot ');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('5', 'Toyota');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('6', 'Nissan');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('7', 'Mitsubishi');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('8', 'Subaru');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('9', 'Chevrolet ');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('10', 'JaC Motors');
+INSERT INTO 'fixxed_database_2' (id_marca, marca) VALUES ('11', 'Renault');
---
+++ app2.js
@@ -0,0 +1,38 @@
+
+const fs = require('fs');// Biblioteca node
+
+function readFile(filename) {
+  let arquiJson = fs.readFileSync(filename); //Lendo Arquivo jason
+  return JSON.parse(arquiJson);
+}
+
+function fixedCharacters(database, key) { // Função responsável por fazer a mudança dos caracteres
+  Object.keys(database).forEach(element => {
+    database[element][key] = database[element][key].replace(/ø/g, 'o').replace(/æ/g, 'a');
+  })
+  return database;
+}
+
+function convertSells(dataBase, key) { // Função responsável por converter o valor numeral de string para int
+  Object.keys(dataBase).forEach(element => {
+    dataBase[element][key] = parseInt(dataBase[element][key]);
+  })
+
+  return dataBase;
+}
+
+function writeFiles(filename, data) {  // Função responsável por exportar os arquivos JSON corrigidos.
+  fs.writeFileSync(filename, JSON.stringify(data));
+}
+
+// Var responsável por chamar as funções que irão fazer alteração no arqui database_1.
+let dataBase = readFile('broken_database_1.json'); 
+dataBase = fixedCharacters(dataBase, 'nome');
+dataBase = convertSells(dataBase, 'vendas');
+ writeFiles('fixxed_database_1.json', dataBase);
+
+
+// Var responsável por chamar as funções que irão fazer alteração no arqui database_2.
+ let dataBase2 = readFile('broken_database_2.json');
+ dataBase2 = fixedCharacters(dataBase2, 'marca');
+ writeFiles('fixxed_database_2.json', dataBase2);
